c1d5a090cd330171764197023179c7829f5022ac
Jordan Ell <me@jordanell.com>
2023-11-07 08:27:21 -0800

    Fixed file dependency

--- lib/copyleaks/models/submissions/properties/indexing_repository.rb
+++ lib/copyleaks/models/submissions/properties/indexing_repository.rb
@@ -21,6 +21,9 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 # =
+
+require_relative  './repository.rb'
+
 module Copyleaks
     class SubmissionIndexingRepository < SubmissionRepository
       # @param [String] ID of a repository to add the scanned document to.
